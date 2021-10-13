0581a13ede68a1adc763e183967d26211c3e5a9b
Dapeng <dapeng_lin@hotmail.com>
2021-10-13 15:03:21 +0800

    url configuration

--- src/main/java/test/springboot/restclientreactive/service/Producer.java
+++ src/main/java/test/springboot/restclientreactive/service/Producer.java
@@ -1,5 +1,6 @@
 package test.springboot.restclientreactive.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -7,8 +8,11 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Component
 public class Producer {
 
+    @Value("${api.sgx.url:https://api.sgx.com}")
+    String sgxApiUrl;
+
     @Bean("api.sgx")
     public WebClient getWebClient(WebClient.Builder builder) {
-        return builder.baseUrl("https://api.sgx.com").build();
+        return builder.baseUrl(sgxApiUrl).build();
     }
 }
